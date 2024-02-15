043fbfa58dea4b1de4529cdb6e8bd804d2c7b88d
Brenden Koenigsman <brenden.koenigsman@gmail.com>
2024-02-15 10:59:07 -0600

    removed clear local storage button for debugging

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,6 @@ import Logo from '../logo/Logo';
 export default function Header() {
   return (
     <header className='header-main'>
-      <button onClick={() => localStorage.clear()}>Clear Storage</button>
       <Logo/>
     </header>
   )
